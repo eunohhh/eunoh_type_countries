50055e2fcdfe7009d049546bbb98bfaba29f682c
Eun Oh <bdohhhhh@gmail.com>
2024-06-29 13:50:51 +0900

    240629 code-review-base222

--- src/types/functions.types.ts
+++
@@ -1,9 +0,0 @@
-import { Country } from "./country.types";
-
-export type GetCountries = () => Promise<Country[]>;
-
-export type FetchCountries = () => Promise<void>;
-
-export type SelectCountry = (country: Country) => void;
-
-export type LoadMoreCountries = () => void;
